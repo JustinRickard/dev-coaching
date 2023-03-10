a433909f250479a06a796d5ed9ec26cce56ca1be
Justin Rickard <justin@river.red>
2023-03-10 11:35:39 +0000

    Inheritance and Interfaces

---
+++ DevCoaching/Inheritance_Interfaces/Inheritance_Before.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevCoaching.Inheritance_Interfaces
+{
+    public abstract class Vehicle
+    {
+        public decimal LengthMetres { get; set; }
+        public decimal WidthMetres { get; set; }
+        public decimal WheelAngle { get; set; }
+
+        public void ExampleSharedFunction()
+        {
+
+        }
+
+        public abstract void Steer(decimal wheelAngle);
+    }
+
+    public class Car : Vehicle
+    {
+        public override void Steer(decimal wheelAngle)
+        {
+            WheelAngle = wheelAngle;
+            // Do something car-specific
+        }
+    }
+
+    public class Bus : Vehicle
+    {
+        public override void Steer(decimal wheelAngle)
+        {
+            WheelAngle = wheelAngle;
+            // Do something bus-specific
+        }
+    }
+
+    public class Train : Vehicle
+    {
+        public override void Steer(decimal wheelAngle)
+        {
+            // no logic here
+        }
+
+    }
+
+    public class VehicleExample
+    {
+        private void UseVehicles()
+        {
+            var car = new Car();
+            var bus = new Bus();
+            var train = new Train();
+
+            var vehicles = new List<Vehicle>();
+            vehicles.Add(car);
+
+            foreach (var vehicle in vehicles)
+            {
+                try
+                {
+                    vehicle.Steer(5);
+                }
+                catch
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ DevCoaching/Inheritance_Interfaces/Interfaces_After.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevCoaching.Inheritance_Interfaces
+{
+
+    public interface ISteerable
+    {
+        void Steer(decimal angle);
+    }
+
+    public abstract class Vehicle2
+    {
+        public decimal LengthMetres { get; set; }
+        public decimal WidthMetres { get; set; }
+        
+        public void ExampleSharedFunction()
+        {
+
+        }
+
+        
+    }
+
+    public class Car2 : Vehicle2, ISteerable
+    {
+        public void Steer(decimal angle)
+        {
+            
+        }
+    }
+
+    public class Bus2 : Vehicle2, ISteerable
+    {
+        public void Steer(decimal angle)
+        {
+
+        }
+    }
+
+    public class Train2 : Vehicle2
+    {
+        
+    }
+
+    public class VehicleExample2
+    {
+        private void UseVehicles()
+        {
+            var car = new Car2();
+            var bus = new Bus2();
+            var train = new Train2();
+
+            var vehicles = new List<ISteerable>();
+            vehicles.Add(car);
+            vehicles.Add(bus);
+            
+
+            foreach (var vehicle in vehicles)
+            {
+                try
+                {
+                    vehicle.Steer(5);
+                }
+                catch
+                {
+
+                }
+            }
+        }
+    }
+
+    // Example of Payment Provider interfaces
+
+    public interface IPaymentProvider
+    {
+        void MakePayment(decimal amount);
+    }
+
+    public interface IStripePaymentProvider : IPaymentProvider
+    {
+
+    }
+    public class StripePaymentProvider : IStripePaymentProvider
+    {
+        public void MakePayment(decimal amount)
+        {
+
+        }
+    }
+
+
+    public interface IWorldPayPaymentProvider : IPaymentProvider
+    {
+
+    }
+    public class WorldPayPaymentProvider
+    {
+        public void MakePayment(decimal amount)
+        {
+
+        }
+    }
+
+    /*
+     Dependency Injection registration:
+     IStripePaymentProvider: StripePaymentProvider
+     IWorldPayPaymentProvider: WorldPayPaymentProvider
+    */
+}
+
