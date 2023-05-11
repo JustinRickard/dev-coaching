404ab9a558da8b17279c3b485782193c42d85571
Justin Rickard <justin@river.red>
2023-05-11 13:51:23 +0100

    Builder pattern - after

--- DevCoaching/Builders/After/Builder_After.cs
+++ DevCoaching/Builders/After/Builder_After.cs
@@ -6,7 +6,62 @@ using System.Threading.Tasks;
 
 namespace DevCoaching.Builders.After
 {
-    internal class Builder_After
+    public class SandwichCommand
     {
+        public void OrderSandwich()
+        {
+            var builder = new SandwichBuilder()
+                .AddBread("ciabatta")
+                .AddButter()
+                .AddHam();
+            
+            var order = builder.Build();
+        }
+
+    }
+
+
+    public class SandwichBuilder
+    {
+        private string _breadType;
+        private bool _includeButter;
+        private bool _includeMargerine;
+        private bool _includeOliveOil;
+        private bool _hasHam;
+        private bool _hasMustard;
+
+        public SandwichBuilder AddBread(string type)
+        {
+            _breadType = type;
+            return this;
+        }
+
+        public SandwichBuilder AddButter()
+        {
+            _includeButter = true;
+            return this;
+        }
+
+        public SandwichBuilder AddMargerine()
+        {
+            _includeMargerine = true;
+            return this;
+        }
+
+        public SandwichBuilder AddHam()
+        {
+            _hasHam = true;
+            return this;
+        }
+
+        public string Build()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"Bread: {_breadType}");
+            sb.AppendLine($"Fatty gloop: {(_includeButter ? "Organic British Butter" : "")}, {(_includeMargerine ? "Marg" : "")}, {(_includeMargerine ? "Olive Oil" : "")}");
+
+            return sb.ToString();
+        }
     }
 }
