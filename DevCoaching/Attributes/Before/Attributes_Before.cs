0cb4d25c3c9450b4e11d4d4cbf18090d0e51045a
Justin Rickard <justin@river.red>
2023-06-02 09:43:34 +0100

    Add Tuples and Attributes

---
+++ DevCoaching/Attributes/Before/Attributes_Before.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevCoaching.Attributes.Before
+{
+	public class Order
+	{
+		public decimal Amount { get; set; }
+		public decimal Surcharge { get; set; }
+		public decimal ExtraLevy { get; set; }
+		public decimal TaxFreeDonation { get; set; }
+	}
+
+	public class TaxCalculator
+	{
+		private const decimal VatRate = 0.2M;
+
+		public decimal Calculate(Order order)
+		{
+			// No tax applied to ExtraLevy or TaxFreeDonation.
+			// VAT on Amount and Surcharge
+			return Convert.ToDecimal(
+				(order.Amount * VatRate) +
+				(order.Surcharge * VatRate)
+			);
+		}
+	}
+}
---
+++ DevCoaching/Tuples/Before/Tuples_Before.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevCoaching.Tuples.Before
+{
+	public class ProcessOrderCommand
+	{
+		public Guid OrderId { get; set; }
+	}
+
+
+	public class ProcessOrderCommandHandler
+	{
+		public void Process(ProcessOrderCommand command)
+		{
+			var validationResult = Validate(command);
+
+			foreach (var itemId in validationResult.ItemIds)
+			{
+				// do something with each item
+			}
+
+			UpdateCarrier(validationResult.Carrier);
+		}
+
+		private void UpdateCarrier(object carrier)
+		{
+			// no logic for example purpose
+		}
+
+		private ValidationResult Validate(ProcessOrderCommand command)
+		{
+			// hard-code return values for example purposes
+			var carrier = new { Name = "DHL" };
+			var itemIds = new[] { Guid.NewGuid() };
+
+
+			return new ValidationResult
+			{
+				Carrier = carrier,
+				ItemIds = itemIds,
+			};
+		}
+
+		private class ValidationResult
+		{
+			public object Carrier { get; set; }
+			public Guid[]? ItemIds { get; set; }
+		}
+	}
+}
