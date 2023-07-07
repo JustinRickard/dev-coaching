49f1eadf913cfa260e39960b86d512a14e0729fe
Justin Rickard <justin@river.red>
2023-07-07 11:44:46 +0100

    Add Tuples after

---
+++ DevCoaching/Tuples/After_Tuple/Tuples_After.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevCoaching.Tuples.After_Tuple
+{
+    public class ProcessOrderCommand
+    {
+        public Guid OrderId { get; set; }
+    }
+
+
+    public class ProcessOrderCommandHandler
+    {
+        public void Process(ProcessOrderCommand command)
+        {
+            var validationResult = Validate(command);
+
+            var carrier = validationResult.Item1;
+            var itemids = validationResult.Item2;
+
+            foreach (var itemId in itemids)
+            {
+                // do something with each item
+            }
+
+            UpdateCarrier(carrier);
+        }
+
+        private void UpdateCarrier(object carrier)
+        {
+            // no logic for example purpose
+        }
+
+        private Tuple<object, Guid[]> Validate(ProcessOrderCommand command)
+        {
+            // hard-code return values for example purposes
+            var carrier = new { Name = "DHL" };
+            var itemIds = new[] { Guid.NewGuid() };
+
+
+            return Tuple.Create((object)carrier, itemIds);
+        }
+    }
+
+}
---
+++ DevCoaching/Tuples/After_ValueTuple/ValueTuple_After.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevCoaching.Tuples.After_ValueTuple
+{
+    public class ProcessOrderCommand
+    {
+        public Guid OrderId { get; set; }
+    }
+
+
+    public class ProcessOrderCommandHandler
+    {
+        public void Process(ProcessOrderCommand command)
+        {
+            var (carrier, itemIds) = Validate(command);
+
+            foreach (var itemId in itemIds)
+            {
+                // do something with each item
+            }
+
+            UpdateCarrier(carrier);
+        }
+
+        private void UpdateCarrier(object carrier)
+        {
+            // no logic for example purpose
+        }
+
+        private ValueTuple<object, Guid[]> Validate(ProcessOrderCommand command)
+        {
+            // hard-code return values for example purposes
+            var carrier = new { Name = "DHL" };
+            var itemIds = new[] { Guid.NewGuid() };
+
+
+            return ((object)carrier, itemIds);
+        }
+    }
+}
