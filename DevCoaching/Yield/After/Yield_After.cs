5d9ba8fcb27d7b38892dabe4b7b5bd399c2bcf98
Justin Rickard <justin@river.red>
2023-07-14 11:20:26 +0100

    Add yield return

---
+++ DevCoaching/Yield/After/Yield_After.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevCoaching.Yield.After
+{
+    public class Yield_After
+    {
+        public IEnumerable<int> GetNumbers()
+        {
+            foreach (var number in Enumerable.Range(1, 10))
+            {
+                yield return number;
+            }
+        }
+
+        public async IAsyncEnumerable<int> GetNumbersAsync()
+        {
+            foreach (var number in Enumerable.Range(1, 10))
+            {
+                yield return await GetIntAsync();
+            }
+        }
+
+        public async Task<int> GetIntAsync() => await Task.FromResult(7);
+    }
+}
---
+++ DevCoaching/Yield/Before/Yield_Before.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevCoaching.Yield.Before
+{
+    public class Yield_Before
+    {
+
+        public IEnumerable<int> GetNumbers()
+        {
+            var result = new List<int>();
+
+            foreach (var number in Enumerable.Range(1, 10))
+            {
+                result.Add(number);
+            }
+
+            return result;
+        }
+    }
+}
