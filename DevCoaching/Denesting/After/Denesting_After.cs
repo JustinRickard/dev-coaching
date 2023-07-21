3602fbcef26c8dc561228162d5bff176050ea5f0
Justin Rickard <justin@river.red>
2023-07-21 11:16:22 +0100

    Add denesting

---
+++ DevCoaching/Denesting/After/Denesting_After.cs
@@ -0,0 +1,40 @@
+﻿namespace DevCoaching.Denesting.After;
+
+public class Denesting_After
+{
+    public void ProcessCustomerBatch(InputRecord[] inputRecords, DateTime lastAllowedTimeUtc)
+    {
+        if (inputRecords == null) return;
+
+        if(inputRecords.Any(x => x.IsAdmin)) 
+            throw new Exception("Can't process customer transactions for admins");
+
+        if(inputRecords.SelectMany(x => x.Transactions).Any(t => t.TransactionUtc > lastAllowedTimeUtc))
+            throw new Exception("Time exceeded, aborting batch");
+
+        foreach (var t in inputRecords.SelectMany(x => x.Transactions))
+        {
+            Process(t);
+        }
+    }
+
+    public void Process(Transaction t)
+    {
+
+    }
+
+    public class InputRecord
+    {
+        public bool IsAdmin { get; set; }
+        public string Name { get; set; }
+
+        public Transaction[] Transactions { get; set; }
+
+    }
+
+    public class Transaction
+    {
+        public DateTime TransactionUtc { get; set; }
+        public string Reference { get; set; }
+    }
+}
---
+++ DevCoaching/Denesting/Before/Denesting_Before.cs
@@ -0,0 +1,47 @@
+﻿namespace DevCoaching.Denesting.Before
+{
+    public class Denesting_Before
+    {
+        public async Task ProcessCustomerBatch(InputRecord[] inputRecords, DateTime lastAllowedTimeUtc)
+        {
+            if (inputRecords != null)
+            {
+                foreach (var r in inputRecords)
+                {
+                    if (r.IsAdmin)
+                    {
+                        throw new Exception("Can't process customer transactions for admins");
+                    }
+
+                    foreach (var t in r.Transactions)
+                    {
+                        if (t.TransactionUtc > lastAllowedTimeUtc)
+                            throw new Exception("Time exceeded, aborting batch");
+
+                        Process(t);
+                    }
+                }
+            }
+        }
+
+        public void Process(Transaction t)
+        {
+
+        }
+
+        public class InputRecord
+        {
+            public bool IsAdmin { get; set; }
+            public string Name { get; set; }
+
+            public Transaction[] Transactions { get; set; }
+            
+        }
+
+        public class Transaction
+        {
+            public DateTime TransactionUtc { get; set; }
+            public string Reference { get; set; }
+        }
+    }
+}
