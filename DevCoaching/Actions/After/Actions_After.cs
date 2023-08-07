96183cc7eaa01dfe5a3b09cb579d13fc83a20d2d
Justin Rickard <justin@river.red>
2023-08-07 17:08:17 +0100

    Add actions

---
+++ DevCoaching/Actions/After/Actions_After.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevCoaching.Actions.After
+{
+	public class Order
+	{
+		public string Id { get; set; }
+		public int Status { get; set; }
+		public decimal Amount { get; set; }
+
+	}
+	public class OrderProcessor
+	{
+		private readonly Dictionary<int, Action<decimal>> _doSomethingStatusToActions = new()
+		{
+			{ 1, DoSomething1 },
+			{ 2, DoSomething2 }
+		};
+
+		public void Process(Order order)
+		{
+			// Do something 1
+
+			_doSomethingStatusToActions[order.Status](order.Amount);
+
+			// Do something 3
+		}
+
+		private static void DoSomething1(decimal amount) { }
+		private static void DoSomething2(decimal amount) { }
+	}
+}
---
+++ DevCoaching/Actions/Before/Actions_Before.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevCoaching.Attributes.After;
+
+namespace DevCoaching.Actions.Before
+{
+	public class Order
+	{
+		public string Id { get; set; }
+		public int Status { get; set; }
+		public decimal Amount { get; set; }
+
+	}
+	public class OrderProcessor
+	{
+		public void Process(Order order)
+		{
+			// Do something 1
+
+			switch (order.Status)
+			{
+				case 1:
+					DoSomething1(order.Amount);
+					break;
+				case 2:
+					DoSomething2(order.Amount);
+					break;
+				default: throw new NotImplementedException();
+			}
+
+			// Do something 3
+		}
+
+		private void DoSomething1(decimal amount){}
+		private void DoSomething2(decimal amount) { }
+	}
+}
