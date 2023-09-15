2da422cb2bc78c6990f54aa9141690dadd811972
Justin Rickard <justin@river.red>
2023-09-15 11:17:37 +0100

    Add Enum Flags

---
+++ DevCoaching/Enum_Flags/After/EnumFlagsAfter.cs
@@ -0,0 +1,59 @@
+﻿namespace DevCoaching.Enum_Flags.After
+{
+    public class EnumFlagsAfter
+    {
+        public void OrderIceCream(IceCreamRequest request)
+        {
+            // Save request to database
+            var dbEntity = new IceCreamDbEntity
+            {
+                Cone = request.Cone,
+                Flavour = request.Flavour,
+                Topping = request.Toppings
+            };
+            // _context.SaveChanges();
+            
+        }
+    }
+
+    public class IceCreamDbEntity
+    {
+        public Flavour Flavour { get; set; }
+        public Cone Cone { get; set; }
+        public Topping Topping { get; set; }
+    }
+
+    public class IceCreamRequest
+    {
+        public Flavour Flavour { get; set; }
+        public Cone Cone { get; set; }
+        public Topping Toppings { get; set; }
+    }
+
+    public enum Flavour
+    {
+        Vanilla,
+        Chocolate,
+        RaspberryRipple,
+        MintAndChocolateChip,
+        Strawberry
+    }
+
+    public enum Cone
+    {
+        Cheap,
+        Waffle,
+        ChocolateWaffle,
+    }
+
+    [Flags]
+    public enum Topping
+    {
+        HundredsAndThousands = 1 << 1,
+        Flake = 1 << 2,
+        RaspberryRipple = 1 << 3,
+        Honey = 1 << 4,
+    }
+
+
+}
---
+++ DevCoaching/Enum_Flags/Before/EnumFlagsBefore.cs
@@ -0,0 +1,58 @@
+﻿namespace DevCoaching.Enum_Flags.Before
+{
+    public class EnumFlagsBefore
+    {
+        public void OrderIceCream(IceCreamRequest request)
+        {
+            // Save request to database
+            var dbEntity = new IceCreamDbEntity
+            {
+                Cone = request.Cone,
+                Flavour = request.Flavour,
+                Topping = request.Topping,
+            };
+            // _context.SaveChanges();
+            
+        }
+    }
+
+    public class IceCreamDbEntity
+    {
+        public Flavour Flavour { get; set; }
+        public Cone Cone { get; set; }
+        public Topping Topping { get; set; }
+    }
+
+    public class IceCreamRequest
+    {
+        public Flavour Flavour { get; set; }
+        public Cone Cone { get; set; }
+        public Topping Topping { get; set; }
+    }
+
+    public enum Flavour
+    {
+        Vanilla,
+        Chocolate,
+        RaspberryRipple,
+        MintAndChocolateChip,
+        Strawberry
+    }
+
+    public enum Cone
+    {
+        Cheap,
+        Waffle,
+        ChocolateWaffle,
+    }
+
+    public enum Topping
+    {
+        HundredsAndThousands,
+        Flake,
+        RaspberryRipple,
+        Honey,
+    }
+
+
+}
